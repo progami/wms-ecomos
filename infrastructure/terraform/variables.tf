5289ed681b6ad82bb903e54954f071c304f98590
jarrar <jarrar@targonglobal.com>
2025-07-07 01:41:10 -0500

    chore: Switch to t3.micro instance for AWS free tier
    - Changed default instance type from t3.medium to t3.micro
    - t3.micro provides 2 vCPUs and 1 GiB RAM (sufficient for WMS)
    - Part of AWS free tier to reduce costs

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "instance_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
